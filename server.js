ba3e6433b54685435060c337047b69ac0d4599e1
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2023-12-06 16:56:43 -0400

    quarto commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer } from 'node:http'
+/*import { createServer } from 'node:http'
 
 const server = createServer((request, response) => {
     //console.log('Olá Mundo!')
@@ -8,4 +8,26 @@ const server = createServer((request, response) => {
 
     return response.end()
 })
-server.listen(3333)
+server.listen(3333)*/
+
+//---------------------------------------------
+
+// importando o fastify
+import { fastify } from 'fastify'
+// criando o servidor
+const server = fastify()
+// rota principal
+server.get('/', () => {
+    return 'Olá Mundo!'
+})
+// outras rotas
+server.get('/livro', () => {
+    return 'Paginas de livros!'
+})
+server.get('/artigo', () => {
+    return 'Paginas de artigo!'
+})
+// passanso a porta com objecto
+server.listen({
+    port: 3333,
+})
