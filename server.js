3761142d531a9093c7636b76231c2f6cf458e476
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2023-12-06 16:12:01 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import { createServer } from 'node:http'
+
+const server = createServer((request, response) => {
+    //console.log('Olá Mundo!')
+
+    response.write('Olá Mundo!')
+    response.write('Já me livrei do kasane')
+
+    return response.end()
+})
+server.listen(3333)
+
